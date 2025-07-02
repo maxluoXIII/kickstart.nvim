2cee160ef8b26654270ebc374398eb665e0239b4
Maxiwell Luo <maxluoxiii@gmail.com>
2025-07-01 23:39:04 -0500

    Change colorscheme to solarized

--- init.lua
+++ init.lua
@@ -894,7 +894,21 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
+    end,
+  },
+  {
+    'maxmx03/solarized.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.o.background = 'dark' -- or 'light'
+      require('solarized').setup {
+        transparent = {
+          enabled = true,
+        },
+      }
+      vim.cmd.colorscheme 'solarized'
     end,
   },
 
