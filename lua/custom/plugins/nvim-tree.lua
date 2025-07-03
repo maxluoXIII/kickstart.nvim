fde2fafa39adc5b1e6556233f7d4b1394172a3d8
Maxiwell Luo <maxluoxiii@gmail.com>
2025-07-02 22:48:11 -0500

    Change tree to nvim-tree

---
+++ lua/custom/plugins/nvim-tree.lua
@@ -0,0 +1,9 @@
+return {
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
+  lazy = false,
+  keys = {
+    { '<leader>tt', ':NvimTreeToggle<CR>', desc = 'Toggle Neotree', silent = true },
+  },
+  opts = {},
+}
