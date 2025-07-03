b19aa97da9afb9d8f9e53157d11b03f2618296df
Maxiwell Luo <maxluoxiii@gmail.com>
2025-07-02 23:39:09 -0500

    Set shell for terminal mode

--- init.lua
+++ init.lua
@@ -219,6 +219,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+if vim.fn.has 'win32' > 0 then
+  vim.o.shell = 'nu'
+  vim.opt.shellslash = true
+end
+vim.opt.shellcmdflag = '-c'
+vim.opt.shellquote = ''
+vim.opt.shellxquote = ''
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
