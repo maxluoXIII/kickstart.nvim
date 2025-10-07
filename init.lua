4576c985acda8bd3bfd30cc64fb0d4b2c750ae2e
Maxiwell Luo <maxluoxiii@gmail.com>
2025-10-06 19:04:16 -0500

    Change diagnostics shortcut

--- init.lua
+++ init.lua
@@ -620,6 +620,10 @@ require('lazy').setup({
           --  the definition of its *type*, not where it was *defined*.
           map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 
+          -- Show diagnostics in floating window
+          -- Default is <C-w>d
+          map('gd', vim.diagnostic.open_float, 'Show diagnostics')
+
           -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
           ---@param client vim.lsp.Client
           ---@param method vim.lsp.protocol.Method
