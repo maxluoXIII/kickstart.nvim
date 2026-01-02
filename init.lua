9ac15272add07999e726843044a1c9d0ae9b8f98
Maxiwell Luo <maxluoxiii@gmail.com>
2026-01-01 19:46:49 -0600

    Use modern preset for which-key

--- init.lua
+++ init.lua
@@ -399,6 +399,7 @@ require('lazy').setup({
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     opts = {
+      preset = 'modern',
       -- delay between pressing a key and opening which-key (milliseconds)
       -- this setting is independent of vim.o.timeoutlen
       delay = 0,
